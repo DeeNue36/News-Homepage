244aaf4cb2d2eb2559e69dbd65e55b4316dfb70a
Dugidem Egwuatu <Dugidemeg@gmail.com>
2024-06-23 23:45:21 +0100

    first commit in a while

---
+++ news.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    document.getElementById("mobile-navcontainer").style.width = "80%";
+
+}
+  function closeMenu() {
+    document.getElementById("mobile-navcontainer").style.width = "";
+
+}

3f28f956814a3e09fda16f5399a857cd2ae939f9
Dugidem Egwuatu <Dugidemeg@gmail.com>
2024-06-26 23:40:37 +0100

    menu overlay

--- news.js
+++ news.js
@@ -1,7 +1,9 @@
 function openMenu() {
   document.getElementById("mobile-navcontainer").style.width = "80%";
+  document.getElementById("mobile-navcontainer").style.boxShadow = "-20px 20px 400px 200px rgba(0, 0, 0, 0.6)";
 }
 
 function closeMenu() {
   document.getElementById("mobile-navcontainer").style.width = "";
+  document.getElementById("mobile-navcontainer").style.boxShadow = "";
 }

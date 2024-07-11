d3877b46910470aca0e4fce09cd7eee42c19c609
Dugidem Egwuatu <Dugidemeg@gmail.com>
2024-07-11 12:15:26 +0100

    menu fixes and comments

--- news.js
+++ news.js
@@ -1,9 +1,9 @@
 function openMenu() {
-  document.getElementById("mobile-navcontainer").style.width = "80%";
-  document.getElementById("mobile-navcontainer").style.boxShadow = "-20px 20px 400px 200px rgba(0, 0, 0, 0.6)";
+  document.getElementById("mobile-navcontainer").style.width = "80vw";
+  document.getElementById("mobile-navcontainer").classList.add("box-shadow");
 }
 
 function closeMenu() {
   document.getElementById("mobile-navcontainer").style.width = "";
-  document.getElementById("mobile-navcontainer").style.boxShadow = "";
+  document.getElementById("mobile-navcontainer").classList.remove("box-shadow");
 }
